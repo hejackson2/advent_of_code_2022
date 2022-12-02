648c5baa57362211ca1defdf72e80bea54a87973
Henry Jackson <95002914+hejackson2@users.noreply.github.com>
2022-12-02 10:29:11 -0500

    moved to outcome matrix rather than nested if/then

--- day/02/day02.py
+++ day/02/day02.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 '''
+first half:
+A = Rock, B = Paper, C = Scissors
+X = Rock, Y = Paper, Z = Scissors
+
 single round sore: second column X = 1, Y = 2, z = 3
 total round: single round score + win = lost, 3 = draw, win = 6
 
@@ -12,9 +16,27 @@ import os
 strategyGuide = open('day/02/input')
 # strategyGuide = ['A Y', 'B X', 'C Z']
 
+# X = Lose = 0, Y = Draw = 3, Z = Win = 6
 outcomeScore = {'X': 0, 'Y': 3, 'Z': 6}
 choiceScore = {'R': 1, 'P': 2, 'S': 3}
-equivalentChoice = {'A': 'R', 'B': 'P', 'C': 'S'}
+
+outcomeMatrix = {
+    'X': {              # Lose
+        'A': 'S',           # Scissors loses to Rock
+        'B': 'R',           # Rock loses to Paper
+        'C': 'P'            # Paper loses to Scissors
+    }, 
+    'Y': {              # Draw
+        'A': 'R',           # Rock / Rock
+        'B': 'P',           # Paper / Paper
+        'C': 'S'            # Scissors / Scissors
+    },
+    'Z': {              # Win
+        'A': 'P',           # Paper beats Rock
+        'B': 'S',           # Scissors beats Paper
+        'C': 'R'            # Rock beats Paper
+    }
+}
 
 totalScore = 0
 score = 0
@@ -32,38 +54,8 @@ for line in strategyGuide.readlines():
     score += outcomeScore[k]
 
     # what choice is needed for outcome
-    # draw
-    if k == 'Y':
-        choice = equivalentChoice[j]
-        score += choiceScore[choice]
-
-    # lose
-    elif k == 'X':
-        # rock, choose scissors to lose
-        if j == 'A':
-            score += choiceScore['S']
-
-        # paper, choose rock to lose
-        elif j == 'B':
-            score += choiceScore['R']
-
-        # scissors, choose paper
-        elif j == 'C':
-            score += choiceScore['S']
-
-    # win
-    elif k == 'Z':
-        # rock, chose paper
-        if j == 'A':
-            score += choiceScore['P']
-
-        # paper, choose scissors
-        elif j == 'B':
-            score += choiceScore['S']
-
-        # scissors, chose rock
-        elif j == 'C':
-            score += choiceScore['R']
+    choice = outcomeMatrix[k][j]
+    score += choiceScore[choice]
 
     
     totalScore += score
@@ -71,5 +63,3 @@ for line in strategyGuide.readlines():
 
 print(f'total score: {totalScore}')
 strategyGuide.close()
-
- 
