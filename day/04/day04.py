ace176ec641baf67253553c020829c72991d2a3c
Henry Jackson <95002914+hejackson2@users.noreply.github.com>
2022-12-04 18:24:28 -0500

    day 04 - part two

--- day/04/day04.py
+++ day/04/day04.py
@@ -33,3 +33,48 @@ with open(data) as file:
             total += 1
 
 print(f'part one total: {total}')
+
+
+## part two
+total = 0
+
+with open(data) as file:
+    for line in file.readlines():
+        line = line.strip()
+
+        # split into two range strings
+        rangeList = line.split(',')
+
+        # get startHour and endHour for each section
+        rangeHours = []
+        for item, string in enumerate(rangeList):
+            startHour = int(string.split('-')[0])
+            endHour = int(string.split('-')[1])
+
+            rangeHours.append(startHour)
+            rangeHours.append(endHour)
+
+        overlap = False
+        # first side
+            # lower value in range of second side?
+            # higher value in range of second side?
+        for v in rangeHours[:2]:
+            if v in range(rangeHours[2], rangeHours[3] + 1):
+                overlap = True
+                break
+
+        # second side
+            # lower value in range of second side?
+            # higher value in range of second side?
+        for v in rangeHours[2:]:
+            if v in range(rangeHours[0], rangeHours[1] + 1):
+                overlap = True
+                break
+
+        if overlap == True:
+            total += 1
+
+print(f'part two total: {total}')
+
+
+        
